b15c19dbb9717cc9a66bf7ca59b43e1ad2e0da0c
jingshansong <1204333218@qq.com>
2022-12-02 09:50:50 +0800

    12.2-1测试

---
+++ src/user/UserController.java
@@ -0,0 +1,13 @@
+package user;
+
+/**
+ * @ClassName: UserController
+ * @Description:
+ * @Date: 2022/12/2 8:52
+ * @Author: 景山松
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
